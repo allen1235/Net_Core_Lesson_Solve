b0caa8b1bced8100d83b7c977a807e587cef1e0e
allen.yen <allen.yen@NB-ALLENYEN>
2023-08-08 14:26:57 +0800

    加入專案檔案。

---
+++ Net_Core_Lesson/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Net_Core_Lesson.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
